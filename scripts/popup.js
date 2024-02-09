24943ca5aa6f4447edd58e69dfb5a873f3996662
haro3535 <66617253+haro3535@users.noreply.github.com>
2024-02-09 13:56:10 +0300

    Add to github

---
+++ scripts/popup.js
@@ -0,0 +1,15 @@
+// chrome.storage.local.get('signed_in', (data) => {
+//     if (data.signed_in) {
+//       chrome.action.setPopup({popup: 'popup.html'});
+//     } else {
+//       chrome.action.setPopup({popup: 'popup_sign_in.html'});
+//     }
+//   });
+
+// "use strict";
+
+// document.getElementById('sideButton').addEventListener("click", () => {
+//     chrome.sidePanel.open({
+//         tabId: chrome.tabs.getCurrent()
+//     });
+// })
